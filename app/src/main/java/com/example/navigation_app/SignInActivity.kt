68cdbc08ca3a003c7663139c179e210f04a9cb44
faryz-coder <faryz.coder@gmail.com>
2021-04-11 03:18:16 +0800

    add signin

--- app/src/main/java/com/example/navigation_app/SignInActivity.kt
+++ app/src/main/java/com/example/navigation_app/SignInActivity.kt
@@ -1,12 +1,84 @@
 package com.example.navigation_app
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.getField
+import com.google.firebase.ktx.Firebase
 
 class SignInActivity : AppCompatActivity() {
+
+    val db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.signin_main)
 
+        val buttonSignin: Button = findViewById(R.id.buttonSignin)
+        val username: TextView = findViewById(R.id.si_username)
+        val password: TextView = findViewById(R.id.si_password)
+        val si_register: TextView = findViewById(R.id.si_register)
+        val lay: ConstraintLayout = findViewById(R.id.layoutsignin)
+
+        buttonSignin.setOnClickListener {
+            if (valid()) {
+                db.collection("user").document(username.text.toString())
+                    .get()
+                    .addOnSuccessListener {
+                        val uid = it.getField<String>("username").toString()
+                        if (uid == username.text.toString()) {
+                            val pass = it.getField<String>("password").toString()
+                            if (pass == password.text.toString()) {
+                                val intent = Intent(this, MainActivity::class.java)
+                                startActivity(intent)
+                                finish()
+                            }
+                        }
+                    }
+            }
+        }
+
+        si_register.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
+
+        lay.setOnClickListener {
+            closeKeyBoard(it)
+        }
+
+    }
+
+    private fun valid(): Boolean {
+        var valid = true
+        val username: TextView = findViewById(R.id.si_username)
+        val password: TextView = findViewById(R.id.si_password)
+
+        if (username.text.toString().isEmpty()) {
+            username.error = "Required"
+            valid = false
+        } else {
+            username.error = null
+        }
+
+        if (password.text.toString().isEmpty()) {
+            password.error = "Required"
+            valid = false
+        } else {
+            password.error = null
+        }
+
+        return valid
+    }
+
+    private fun closeKeyBoard(v : View) {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(v.windowToken, 0)
     }
 }
