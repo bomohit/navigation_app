717a717c0b80ecc551ff779c8597ada9f353d00b
faryz-coder <faryz.coder@gmail.com>
2021-04-11 03:13:08 +0800

    initiate project

---
+++ app/src/main/java/com/example/navigation_app/SignInActivity.kt
@@ -0,0 +1,12 @@
+package com.example.navigation_app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.signin_main)
+
+    }
+}
